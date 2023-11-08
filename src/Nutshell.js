027cb50407786dfeb181fea99e4ba0e8265b8507
Christian Montana <christian.montana.39@gmail.com>
2023-11-08 12:09:59 -0500

    ver2CMauth

--- src/Nutshell.js
+++ src/Nutshell.js
@@ -2,61 +2,23 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom"
 import { NavBar } from './components/nav/NavBar';
 import { ApplicationViews } from "./components/Views/ApplicationViews";
-
 import React, { useState } from "react";
-import LoginForm from "./Components/LoginForm";
-
+import { Login } from './components/Auth/LoginForm';
+import { Authorized } from "./components/Views/Authorized";
 function Nutshell() {
-  return <BrowserRouter>
-  <Routes>
-  <Route path="*" element={
-
-      <>
-        <NavBar />
-        <ApplicationViews/>
-      </>
-
-
-  } />
-    </Routes>
-</BrowserRouter>
-
-const adminUser = {
-	email: "example@example.com", 
-	password: "password"
-}
-
-const [user, setUser] = useState({name: "", email: ""});
-const [error, setError] = useState(""); //string to catch if correct
-
-const Login = details => {
-console.log(details);
-
-if (details.email == adminUser.email && details.password == adminUser.password)
-	console.log("Logged in");
-setUser({
-	name: details.name,
-	email: details.email
-});
-} 
-const Logout = () => {
-	setUser({name: "", email: ""});
-}
-
-	return (
-		<div className="Nutshell">
-			{(user.email != "") ? (
-				<div className="welcome">
-					<h2>Welcome</h2>
-					<button onClick ={Logout}>Logout</button>
-					</div>
-			) : (
-				<LoginForm Login={Login} error={error}/>
-			)}
-		</div>
-		
-	);
-}
+	return <Routes>
+	<Route path="/login" element={<Login />} />  
+	<Route path="*" element={
+	  <Authorized>
+		<>
+		  <NavBar />
+		  <ApplicationViews />
+		</>
+	  </Authorized>
+  
+	} />
+  </Routes>
+  }
 
 export default Nutshell;
 
